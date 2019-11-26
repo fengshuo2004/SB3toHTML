69c7f658e73158a3d565363b9483b9244faf8a9d
Feng Shuo <fengshuo2004@163.com>
2019-11-26 20:06:56 +0000

    removed all unnesscery files

--- README.md
+++
@@ -1,2 +0,0 @@
-# words-go-here
-An attempt to allow people to put their text on a screen even when better alternatives exist
--- misc/spam.js
+++
@@ -1,3 +0,0 @@
-var SHEP={};
-SHEP.t=document.querySelector('[name="1d7z9npyy82e"]');
-SHEP.t.contentDocument.onclick=console.log;
