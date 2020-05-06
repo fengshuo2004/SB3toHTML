97c853e8fa67863736caa5bab1ffad5e3227adee
Feng Shuo <fengshuo2004@163.com>
2020-05-07 00:23:58 +0800

    ready to build

--- README.md
+++ README.md
@@ -1,15 +1,44 @@
-# Scratch 3 to HTML Converter
+# Scratch 3 到 HTML 转换器
 
-![screenshot of the converter running](resources/screenshot.png)
+## 总览
 
-> ⚠ WARNING: WORK IN PROGRESS! NOT YET USABLE! CHINESE LANG UI ONLY!
+SB3toHTML通过将一个Scratch项目连同所需的scratch虚拟机打包在一起，将这个项目转换成独立的单个html文件。
 
-Core code forked from **Sheep_maker**'s repository, this program converts Scratch 3 projects (.sb3 files) into stand-alone HTML files.
+相比于分发sb3项目文件，直接分发html文件解决了需要额外安装Scratch编辑器的不便（仍然需要支持HTML5的网页浏览器）；同时也能提高抄袭源码的成本，一定程度上保护您的项目。
 
-I used NodeJS Electron to pack this program into a Windows/MacOS/Linux Executable, along with a cosmetic overhual of the original UI.
-
-> ⚠ 警告：仍在开发过程中，尚不能运行！界面语言仅限中文！
+![screenshot of the converter running](assets/screenshot.png)
 
 核心代码分叉自Sheep_maker的仓库，本程序把Scratch 3项目（.sb3文件）转换为独立的HTML文件。
 
 我使用NodeJS Electron将此程序打包成Windows/MacOS/Linux执行档，还顺便对原本的用户界面进行了美观性打磨。
+
+## 开发
+
+如果您想自己开发和构建本项目，您需要NodeJS环境和NPM包管理器。遵循以下步骤：
+
+1. 用git克隆这个仓库到本地：
+
+```bash
+git clone https://github.com/fengshuo2004/SB3toHTML.git
+```
+
+2. NPM安装需要的依赖：
+
+```bash
+cd SB3toHTML
+npm install
+```
+
+3. 启动Electron：
+
+```bash
+electron .
+```
+4. 完成！
+
+## 计划
+
+将来会支持这些新功能（按重要度从大到小排列)
+
+- [ ] 支持拖动文件进窗口作为sb3项目
+- [ ] 将分支更新到与SheepTester的Master平行，这会带来如连接到云变量服务器、加载中占位图等新功能
--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+const license = `版权所有 © 2019-2020 冯铄及Pixelworld Studio
+
+特此免费授予获得此软件和相关文档文件（“软件”）副本的任何人无限制使用软件的权利，包括但不限于使用，复制，修改，合并的权利，发布，分发，再许可和/或出售本软件的副本，并允许具备软件的人员这样做，但须满足以下条件：
+
+上述版权声明和此许可声明应包含在本软件的所有副本或大部分内容中。
+
+本软件按“原样”提供，不提供任何形式的明示或暗示的保证，包括但不限于对适销性，特定目的的适用性和非侵权性的保证。无论是由于软件，使用或其他方式产生的，与之有关或与之有关的合同，侵权或其他形式的任何索赔，损害或其他责任，作者或版权所有者概不负责。`;
+
+const aboutElectron = `本程序使用了Electron v8.2.5
+
+Electron利用Chromium渲染引擎和NodeJS运行时实现基于JavaScript，HTML和CSS等网页前端技术的跨平台桌面应用并可为其自动生成安装包。
+
+更多信息请见官网 https://www.electronjs.org`;
+
 const electron = require('electron');
 const _app = electron.app;
 const _BrowserWindow = electron.BrowserWindow;
@@ -10,7 +24,7 @@ var mainWindow = null;
 // ========== Menu Bar ==========
 const template = [
     {
-        label: "视图",
+        label: "查看(V)",
         submenu: [
             {
                 label: "放大",
@@ -30,7 +44,18 @@ const template = [
         ]
     },
     {
-        label: "关于",
+        label: "帮助(H)",
+        submenu: [
+            {
+                label: "联机帮助文档...",
+                click() {
+                    electron.shell.openExternal("https://github.com/fengshuo2004/SB3toHTML/wiki");
+                }
+            }
+        ]
+    },
+    {
+        label: "关于(A)",
         submenu: [
             {
                 label: "原作者SheepTester...",
@@ -45,21 +70,15 @@ const template = [
                 }
             },
             {
-                label: "DownloadJS...",
+                label: "Electron框架...",
                 click() {
-                    electron.shell.openExternal("http://danml.com/download.html");
-                }
-            },
-            {
-                label: "Electron...",
-                click() {
-                    electron.shell.openExternal("https://www.electronjs.org/");
+                    electron.dialog.showMessageBox(mainWindow, { type: "info", message: aboutElectron, icon: "assets/electron.ico" });
                 }
             },
             {
                 label: "MIT开源软件许可证...",
                 click() {
-                    electron.shell.openItem('LICENSE');
+                    electron.dialog.showMessageBox(mainWindow, { type: "info", message: license, icon: "assets/icon.ico"});
                 }
             }
         ]
@@ -97,11 +116,11 @@ _app.on('ready', function () {
     const appMenu = _Menu.buildFromTemplate(template);
     _Menu.setApplicationMenu(appMenu);
     // 创建浏览器窗口。
-    mainWindow = new _BrowserWindow({ width: 750, height: 890 , icon:"resources/icon.png"});
+    mainWindow = new _BrowserWindow({ width: 750, height: 890 , icon:"assets/icon.ico"});
 
     // 加载应用的 index.html
     mainWindow.loadURL('file://' + __dirname + '/index.html');
-
+    mainWindow.openDevTools();
     // 当 window 被关闭，这个事件会被发出
     mainWindow.on('closed', function () {
         // 取消引用 window 对象，如果你的应用支持多窗口的话，
